ddc0b2c179eb41634a7c3425383b364353f0d69a
EricBrisola <eric.coca13@gmail.com>
2024-09-27 10:58:51 -0300

    chore: working with session token

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Login, SignUp, AddExpense } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [token, setToken] = useState(false);
+
+  if (token) {
+    localStorage.setItem("token", JSON.stringify(token));
+  }
+
+  // useEffect(() => {
+  //   if (localStorage.getItem("token")) {
+  //     const data = JSON.parse(localStorage.getItem("token"));
+  //     setToken(data);
+  //   }
+  // }, []);
+
   return (
     <Routes>
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Login setToken={setToken} />} />
       <Route path="/add-expense" element={<AddExpense />} />
     </Routes>
   );
