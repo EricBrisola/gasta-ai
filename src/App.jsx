fda9436d9578cecb5fb30258646543c28e7a4716
EricBrisola <eric.coca13@gmail.com>
2024-10-12 16:25:33 -0300

    refactor: removed setToken prop and localstorage to save user

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,10 @@ import { Login, SignUp, AddExpense } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [token, setToken] = useState(false);
-
-  if (token) {
-    localStorage.setItem("token", JSON.stringify(token));
-  }
-
-  // useEffect(() => {
-  //   if (localStorage.getItem("token")) {
-  //     const data = JSON.parse(localStorage.getItem("token"));
-  //     setToken(data);
-  //   }
-  // }, []);
-
   return (
     <Routes>
       <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/" element={<Login setToken={setToken} />} />
+      <Route path="/" element={<Login />} />
       <Route path="/add-expense" element={<AddExpense />} />
     </Routes>
   );
