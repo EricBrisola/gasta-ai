ab09184253aae701dd43a2eefa4e6da8e1b645c3
EricBrisola <eric.coca13@gmail.com>
2024-10-16 23:45:58 -0300

    feat: changed user functions from supabase to firebase

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -1,171 +1,169 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import useRedirect from "../hooks/useRedirect";
-import supabase from "../API/client";
+import { auth, db } from "../API/firebase";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   const redirectTo = useRedirect();
 
-  const saveUser = async () => {
-    try {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
+  useEffect(() => {
+    if (user) {
+      getUserData(user?.uid);
+      console.log(userData);
+    }
+  }, [user]);
 
-      if (user) {
-        setUser(user);
-      }
-      console.log(user);
-    } catch (error) {
-      alert(`Erro: ${error}`);
+  const getUserData = async (id) => {
+    //id é o nome/id do documento que voce quer receber
+    const docSnap = await getDoc(doc(db, "users", id));
+
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      setUserData(docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No user found!");
     }
   };
 
-  const loginUser = async (
-    ev,
-    startLoading,
-    stopLoading,
-    setFormData,
-    email,
-    password,
-  ) => {
+  //Entrar com um usuario que ja existe email/senha
+  const loginUser = async (ev, startLoading, stopLoading, formData) => {
     ev.preventDefault();
+
     try {
       startLoading();
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      });
+      const newUser = await signInWithEmailAndPassword(
+        auth,
+        formData.email,
+        formData.password,
+      );
+      setUser(newUser.user);
+      console.log(user);
 
-      if (error) {
-        console.error("Login error:", error); // Adicionar log de erro
-        alert("Erro ao fazer login: " + error.message);
-        stopLoading();
-      }
-      console.log(data);
-      setFormData({
-        email: "",
-        password: "",
-      });
-
-      if (data) {
-        await saveUser();
+      if (newUser.user) {
         redirectTo("/add-expense");
+      }
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+      stopLoading();
+    }
+  };
+
+  // const loginWithGoogle = async (startLoading, stopLoading) => {
+  //   try {
+  //     startLoading();
+  //     const { data, error } = await supabase.auth.signInWithOAuth({
+  //       provider: "google",
+  //       options: {
+  //         redirectTo: "http://localhost:5173/add-expense",
+  //       },
+  //     });
+  //     console.log(data);
+
+  //     if (data) await saveUser();
+
+  //     if (error) {
+  //       console.error("Login error:", error); // Adicionar log de erro
+  //       alert("Erro ao fazer login: " + error.message);
+  //       stopLoading();
+  //     }
+  //   } catch (error) {
+  //     alert("Erro ao tentar conectar com google");
+  //   }
+  // };
+
+  const loginWithGoogle = async (startLoading, stopLoading) => {
+    //startLoading();
+    //TODO: ver como fazer para usar com redirect
+    const provider = new GoogleAuthProvider();
+    try {
+      const result = await signInWithPopup(auth, provider);
+
+      // Obtém o token de acesso do Google para acessar a API do Google
+      //const credential = GoogleAuthProvider.credentialFromResult(result);
+      // Informações do usuário logado
+      const user = result.user;
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    } catch (error) {
+      // Trate os erros aqui
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      stopLoading();
+    }
+  };
+
+  const signupUser = async (ev, startLoading, formData, stopLoading) => {
+    ev.preventDefault();
+    //chama o loading aqui e no login pois se deixasse só o do login ele tem um delay estranho para fazer o loading, rever futuramente
+    startLoading();
+
+    try {
+      if (formData.password != formData.passwordConfirm) {
+        throw new Error("Confirme sua senha corretamente!");
+      }
+
+      if (formData.password.length < 6) {
+        throw new Error("A senha deve ter no mínimo 6 caracteres.");
+      }
+
+      const newUser = await createUserWithEmailAndPassword(
+        auth,
+        formData.email,
+        formData.password,
+      );
+      const userUid = newUser.user.uid;
+
+      const docData = {
+        uid: userUid,
+        email: formData.email,
+        name: formData.name,
+      };
+      await setDoc(doc(db, "users", userUid), docData);
+      if (newUser) {
+        loginUser(ev, startLoading, stopLoading, formData);
       }
     } catch (error) {
       alert(error);
     }
   };
 
-  const loginWithGoogle = async (startLoading, stopLoading) => {
-    try {
-      startLoading();
-      const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-          redirectTo: "http://localhost:5173/add-expense",
-        },
-      });
-      console.log(data);
-
-      if (data) await saveUser();
-
-      if (error) {
-        console.error("Login error:", error); // Adicionar log de erro
-        alert("Erro ao fazer login: " + error.message);
-        stopLoading();
-      }
-      //TODO: verificar a necessidade desse if e talvez trocar por um throw new error
-    } catch (error) {
-      alert("Erro ao tentar conectar com google");
-    }
-  };
-
-  const signupUser = async (
-    ev,
-    startLoading,
-    email,
-    password,
-    passwordConfirm,
-    name,
-    setFormData,
-  ) => {
-    ev.preventDefault();
-    try {
-      if (password != passwordConfirm) {
-        throw new Error("Confirme sua senha corretamente!");
-      }
-
-      if (password.length < 6) {
-        throw new Error("A senha deve ter no mínimo 6 caracteres.");
-      }
-
-      startLoading();
-
-      const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password,
-        options: {
-          data: {
-            name: name,
-          },
-        },
-      });
-
-      if (error) throw new Error(error);
-
-      alert(
-        "Conta criada com sucesso! Cheque seu email e acesse o link de verificação.",
-      );
-      setFormData({
-        name: "",
-        email: "",
-        password: "",
-        passwordConfirm: "",
-      });
-      if (data) {
-        await saveUser();
-        redirectTo("/add-expense");
-      }
-    } catch (error) {
-      alert(`${error}`);
-    }
-  };
-
-  const checkUserSession = async () => {
-    try {
-      const { data, error } = await supabase.auth.getSession();
-      if (error) {
-        throw error;
-      }
-      if (data.session) {
-        setUser(data.session.user);
-      }
-    } catch (error) {
-      console.error("Erro ao verificar sessão:", error);
-    }
-  };
-
   const logoutUser = async () => {
     try {
-      const { error } = await supabase.auth.signOut();
-      setUser(null);
-      if (error) throw new Error();
+      await signOut(auth);
+      console.log("deslogou");
     } catch (error) {
-      alert(`Erro: ${error}`);
+      console.log(error);
     }
   };
-
-  useEffect(() => {
-    checkUserSession();
-  }, []);
 
   return (
     <UserContext.Provider
-      value={{ user, loginUser, loginWithGoogle, signupUser, logoutUser }}
+      value={{
+        user,
+        userData,
+        setUser,
+        loginUser,
+        loginWithGoogle,
+        signupUser,
+        logoutUser,
+      }}
     >
       {children}
     </UserContext.Provider>
