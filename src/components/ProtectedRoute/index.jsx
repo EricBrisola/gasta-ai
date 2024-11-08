0a4a7769fa12895ecd584a9f9702fc682c9ac665
EricBrisola <eric.coca13@gmail.com>
2024-11-07 22:58:32 -0300

    fix: now protected route is properly working and added a loading anime

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,23 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import useRedirect from "../../hooks/useRedirect";
-import { auth } from "../../API/firebase";
 import { useUser } from "../../hooks/useUser";
+import Modal from "../Modal";
+import Animation from "../Animation";
+import loadingAnimation from "../../assets/loading_animation.json";
 
+// eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ children }) => {
   const redirectTo = useRedirect();
-  const { setUser } = useUser();
+  const { user, loading } = useUser(); // Usar o estado de `user` diretamente do contexto
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (!user) {
-        setUser(null);
-        redirectTo("/");
-      }
-    });
-    return () => unsubscribe();
-  }, []);
+    if (!loading && !user) {
+      redirectTo("/"); // Redireciona para a página de login apenas quando não está carregando e o usuário não está autenticado
+    }
+  }, [user, loading]);
 
-  return children; // Renderiza o conteúdo protegido se o usuário estiver autenticado
+  if (loading)
+    return (
+      <Modal>
+        <Animation animation={loadingAnimation} />
+      </Modal>
+    ); // Mostra uma tela de carregamento enquanto verifica a autenticação
+
+  return user && children; // Renderiza o conteúdo protegido se o usuário estiver autenticado
 };
 
 export default ProtectedRoute;
