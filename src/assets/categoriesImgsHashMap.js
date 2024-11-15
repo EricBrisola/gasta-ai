278d6aa27cd4ee8a9d4244c78791a5a63f3e3dcf
EricBrisola <eric.coca13@gmail.com>
2024-11-15 19:07:52 -0300

    feat: created hashmap with categories images

---
+++ src/assets/categoriesImgsHashMap.js
@@ -0,0 +1,17 @@
+import foodIcon from "./food-icon.png";
+import transportIcon from "./transport-icon.png";
+import enterteinmentIcon from "./enterteinment-icon.png";
+import homeIcon from "./home-icon.png";
+import healthIcon from "./health-icon.png";
+import miscIcon from "./miscellaneous-icon.png";
+
+const categoriesImgHashMap = {
+  food: foodIcon,
+  transport: transportIcon,
+  enterteinment: enterteinmentIcon,
+  home: homeIcon,
+  health: healthIcon,
+  miscellaneous: miscIcon,
+};
+
+export default categoriesImgHashMap;
