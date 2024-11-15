a06dfb2d949071c9ae147ffa28baf5de82e5b51e
EricBrisola <eric.coca13@gmail.com>
2024-11-15 19:09:11 -0300

    feat: removed loader when verifying if user is logged

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -2,9 +2,6 @@
 import { useEffect } from "react";
 import useRedirect from "../../hooks/useRedirect";
 import { useUser } from "../../hooks/useUser";
-import Modal from "../Modal";
-import Animation from "../Animation";
-import loadingAnimation from "../../assets/loading_animation.json";
 
 // eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ children }) => {
@@ -16,13 +13,6 @@ const ProtectedRoute = ({ children }) => {
       redirectTo("/"); // Redireciona para a página de login apenas quando não está carregando e o usuário não está autenticado
     }
   }, [user, loading]);
-
-  if (loading)
-    return (
-      <Modal>
-        <Animation animation={loadingAnimation} />
-      </Modal>
-    ); // Mostra uma tela de carregamento enquanto verifica a autenticação
 
   return user && children; // Renderiza o conteúdo protegido se o usuário estiver autenticado
 };
