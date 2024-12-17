fb7ebaae7898c9363508b9cebc466e066ea4ad46
EricBrisola <eric.coca13@gmail.com>
2024-12-17 00:13:31 -0300

    refactor: created useModal hook and categories file

---
+++ src/utils/categories.js
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    name: "Alimentação",
+    value: "food",
+  },
+  {
+    name: "Transporte",
+    value: "transport",
+  },
+  {
+    name: "Entretenimento",
+    value: "enterteinment",
+  },
+  {
+    name: "Moradia",
+    value: "home",
+  },
+  {
+    name: "Saúde",
+    value: "health",
+  },
+  {
+    name: "Diversos",
+    value: "miscellaneous",
+  },
+];
