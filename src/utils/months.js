d702b18e27fee7a928ecd9c47a9f93d65c63ec6c
EricBrisola <eric.coca13@gmail.com>
2025-01-23 17:00:24 -0300

    remove: deleted unused files

---
+++ src/utils/months.js
@@ -0,0 +1,16 @@
+import dayjs from "dayjs";
+
+export const months = [
+    { month: "Janeiro", value: dayjs().startOf() },
+    { month: "Fevereiro", value: "02" },
+    { month: "Março", value: "03" },
+    { month: "Abril", value: "04" },
+    { month: "Maio", value: "05" },
+    { month: "Junho", value: "06" },
+    { month: "Julho", value: "07" },
+    { month: "Agosto", value: "08" },
+    { month: "Setembro", value: "09" },
+    { month: "Outubro", value: "10" },
+    { month: "Novembro", value: "11" },
+    { month: "Dezembro", value: "12" },
+];
