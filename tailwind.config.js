cb761f768ff0c8755e344214c42f98cc737ab0f3
EricBrisola <eric.coca13@gmail.com>
2025-02-26 00:31:41 -0300

    chore: removed supabase and added lucide-react lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        "auto-fit": "repeat(auto-fit,minmax(12rem,1fr))",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
