e012cc165f587784555345de17cede41bd9c8842
EricBrisola <eric.coca13@gmail.com>
2025-04-10 21:31:55 -0300

    fixed form position on addExpense page

--- src/pages/AddExpense.jsx
+++ src/pages/AddExpense.jsx
@@ -5,7 +5,7 @@ export const AddExpense = () => {
   return (
     <main className="flex h-screen flex-col">
       <Navbar />
-      <section className="flex flex-1 flex-col items-center justify-center gap-7 bg-[#E2DEE9] text-[#102a42]">
+      <section className="flex flex-1 flex-col items-center justify-center gap-7 bg-[#E2DEE9] text-[#102a42] max-[425px]:gap-4 max-[425px]:pt-10">
         <p className="text-2xl leading-none tracking-widest text-[#102a42]">
           Registre seus gastos!
         </p>
